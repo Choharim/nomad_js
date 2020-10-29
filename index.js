d1130521e2e310cc694b3ecaffb51f65bc795e7a
Choharim <jhl8586@naver.com>
2020-10-29 13:42:59 +0900

    Add array,object

--- index.js
+++ index.js
@@ -25,3 +25,14 @@ const familyOfHarim = ["father","mother","sister","mori",true,5,0.5]
 console.log(familyOfHarim);
 console.log(familyOfHarim[0]);
 console.log(familyOfHarim[4]);
+
+const friendsOfMori = 
+{ parang: "blue bear",
+  bunhong:true, 
+  snack: ["sweet potato","bam"],
+  fav: [{one: 1,two: 2},{three: 3,four: 4}],
+  pav: {double: [1,2],tri: [1,2,3]}
+};
+console.log(friendsOfMori.fav[0].one);
+console.log(friendsOfMori.pav.tri[0]);
+console.log(friendsOfMori.fav[0].one);
