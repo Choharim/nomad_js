94f2a7684a35dd8a9944f4e0fa643a3a2d89e042
Choharim <jhl8586@naver.com>
2020-10-29 13:12:43 +0900

    Add let,const

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 console.log('Im working.');
 
+let a = 10;
+let b = a / 2 ;
+console.log(a,b);
 
 
 
+
+let e = 2;
+e=4;
+let f = e+2;
+console.log(f,e);
+
+let c = 2;
+let d = c+2;
+c=4;
+console.log(d,c);
+
+const g = 20;
+const h = g*2;
+console.log(g,h);
