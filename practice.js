c4e8bc714f3245fda62fcf17a422870926384a96
Choharim <jhl8586@naver.com>
2020-10-29 17:02:52 +0900

    Add window,function

--- practice.js
+++ practice.js
@@ -1,2 +1,12 @@
 const title = document.getElementById("text");
 title.innerHTML = "hi im js";
+console.dir(title);
+
+title.style.color = "pink";
+document.querySelector("#title");
+
+
+function click () {
+  title.style.color = "blue";
+}
+window.addEventListener("click",click);
